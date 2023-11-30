3b92ee69986d046b7b4fddbf39c2b17c9f9ef846
Elisa W <elisajogja@gmail.com>
2023-11-30 08:40:19 +0700

    Tambah UserController dan UserSeeder + LTE

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        $data = User::get();
+
+        return view('index',compact('data'));
+    }
+}
+
